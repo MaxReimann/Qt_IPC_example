40599e0b444575e0999b368d4ef9a74ee26871d0
MaxReimann <max.reimann@student.hpi.uni-potsdam.de>
2016-01-17 00:35:35 +0100

    add code for bidirectional image sending.. makes this a lot more complicated

--- src/process_example.cpp
+++ src/process_example.cpp
@@ -3,6 +3,88 @@
 #include <QBuffer>
 #include <QImage>
 #include <QSharedMemory>
+#include <QPoint>
+
+#define PROCESSNAME QString("process_example")
+
+void copyToMemory(QSharedMemory& memory, QBuffer& buf, int size)
+{
+	memory.lock();
+    char *to = (char*)memory.data();
+    const char *from = buf.data().data();
+    memcpy(to, from, qMin(memory.size(), size));
+    memory.unlock();
+}
+
+
+QPoint readCommStatus(QSharedMemory& memory)
+{
+	QPoint status;
+	QBuffer buf;
+    QDataStream stream_recv(&buf);
+
+    memory.lock();
+    buf.setData((char*)memory.constData(), memory.size());
+    buf.open(QBuffer::ReadOnly);
+    stream_recv >> status;
+    memory.unlock();
+
+    return status;
+}
+
+void updateCommStatus(QSharedMemory& memory, int status)
+{
+
+	QPoint entireStatus = readCommStatus(memory);
+	entireStatus.setX(status);
+
+
+	QBuffer buf;
+    buf.open(QBuffer::ReadWrite);
+	QDataStream out(&buf);
+	out << entireStatus;
+
+	copyToMemory(memory, buf, buf.size());
+}
+
+void updateCommStatusClient(QSharedMemory& memory, int clientStatus)
+{
+
+	QPoint entireStatus = readCommStatus(memory);
+	entireStatus.setY(clientStatus);
+
+
+	QBuffer buf;
+    buf.open(QBuffer::ReadWrite);
+	QDataStream out(&buf);
+	out << entireStatus;
+
+	copyToMemory(memory, buf, buf.size());
+}
+
+
+int waitAndPrint(QProcess* pyProcess, QSharedMemory& sharedMemory_comm)
+{
+	int pending = 0;
+    while (pyProcess->waitForReadyRead(-1) && pending == 0){
+		QByteArray newData = pyProcess->readAllStandardOutput();
+		QString result = QString::fromLocal8Bit(newData);
+		qDebug(qPrintable(QString("py:")+result));
+
+		QByteArray errordata = pyProcess->readAllStandardError();
+		QString resultErr = QString::fromLocal8Bit(errordata);
+		if (QString::compare(resultErr,QString("")) != 0)
+		{
+			qDebug(qPrintable(QString("py error:")+resultErr));
+			qDebug() << "exting because of python error..";
+			return -1;
+		}
+
+		pending = readCommStatus(sharedMemory_comm).y();
+	}
+
+	return pending;
+}
 
 int main(int argc, const char* argv[])
 {
@@ -13,6 +95,27 @@ int main(int argc, const char* argv[])
     	qDebug() << " image could not be loaded";
     	return 0;
     }
+
+
+    QBuffer buffer_comm;
+    buffer_comm.open(QBuffer::ReadWrite);
+    QDataStream out_comm(&buffer_comm);
+    //pair stands for 1: host memory (_in memory) pending buffer size (unused) 
+    //2: pyprocess memory pending buffer size (_out memory)
+    out_comm << QPoint(0,0); 
+
+
+    QSharedMemory sharedMemory_comm(PROCESSNAME + QString("_communication"));
+    if (!sharedMemory_comm.create(buffer_comm.size())) {
+        qDebug() << "Unable to create shared memory segment for communication memory.";
+        return 0;
+    }
+
+  
+    copyToMemory(sharedMemory_comm, buffer_comm, buffer_comm.size());
+
+
+
 
     QString program = "python";
     QStringList arguments;
@@ -27,35 +130,63 @@ int main(int argc, const char* argv[])
     buffer.open(QBuffer::ReadWrite);
     QDataStream out(&buffer);
     out << image;
+    // out << image2;
 
     int size = buffer.size();
 
-    QSharedMemory sharedMemory("process_example");
+    QSharedMemory sharedMemory(PROCESSNAME + QString("_in"));
     if (!sharedMemory.create(size)) {
         qDebug() << "Unable to create shared memory segment.";
         return 0;
     }
 
     qDebug() << "sizeof mem: " << sharedMemory.size() << "bufsize:" << buffer.size();
-    sharedMemory.lock();
-    qDebug() << "shared memory locked";
 
-
-    char *to = (char*)sharedMemory.data();
-    const char *from = buffer.data().data();
-    memcpy(to, from, qMin(sharedMemory.size(), size));
-    
-    sharedMemory.unlock();
+    copyToMemory(sharedMemory, buffer, size);
 
     qDebug() << "image copied to shared memory";
-
-    while (pyProcess->waitForReadyRead(-1)){
-		QByteArray newData = pyProcess->readAllStandardOutput();
-		QString result = QString::fromLocal8Bit(newData);
-		qDebug(qPrintable(QString("py:")+result));
-	}
-
+    int result = waitAndPrint(pyProcess,sharedMemory_comm);
 	sharedMemory.detach();
+
+    if (result < 0)
+    	return -1;
+
+  	int pending = result;
+
+	qDebug() << "ok now to second part..";
+
+	QSharedMemory sharedMemory_recv(PROCESSNAME + QString("_out"));
+
+
+    if (!sharedMemory_recv.create(pending)) {
+        qDebug() << "Unable to create shared memory segment.";
+		qDebug() << sharedMemory_recv.errorString();
+        return 0;
+    }
+    qDebug() << "pending" << sharedMemory_recv.size();
+	// set pending data back to 0
+	updateCommStatusClient(sharedMemory_comm, 0);
+
+
+	QBuffer buffer_recv;
+    QDataStream stream_recv(&buffer_recv);
+    QImage image_in;
+
+    result = waitAndPrint(pyProcess,sharedMemory_comm);
+
+    sharedMemory_recv.lock();
+    buffer_recv.setData((char*)sharedMemory_recv.constData(), sharedMemory_recv.size());
+    buffer_recv.open(QBuffer::ReadOnly);
+    stream_recv >> image_in;
+    sharedMemory_recv.unlock();
+
+
+    
+    qDebug() << "received image width" << image_in.width();
+
+    sharedMemory_recv.detach();
+    sharedMemory_comm.detach();
+
 
 	return 0;
 }
--- test.py
+++ test.py
@@ -1,35 +1,137 @@
 from PyQt5 import QtCore, QtGui
+import numpy as np
 
 
-_memory = QtCore.QSharedMemory()
-_memory.setKey('process_example')
-attached = False
-while not attached:
-	if _memory.attach():
-		print("memory attached")
-		attached = True
+class IPCHandler(object):
+	def __init__(self, process_name):
+		self.in_memory = QtCore.QSharedMemory()
+		self.in_memory.setKey(process_name+"_in")
+		while True:
+			if self.in_memory.attach():
+				print("memory attached")
+				break
+		self.images = []
+
+		self.out_memory = QtCore.QSharedMemory()
+		self.out_memory.setKey(process_name+"_out")
+
+		self.comm_memory = QtCore.QSharedMemory()
+		self.comm_memory.setKey(process_name + "_communication")
+		while True:
+			if self.comm_memory.attach():
+				print("comm memory attached")
+				break
+
+	def receiveImages(self):
+		self.images = []
+		buf = QtCore.QBuffer()
+		datastream = QtCore.QDataStream(buf)
+
+		self.in_memory.lock();
+		buf.setData(self.in_memory.constData())
+		buf.open(QtCore.QBuffer.ReadOnly)
+
+		while True:
+			qimg = QtGui.QImage()
+			try:
+				datastream >> qimg
+			except Exception as e:
+				print(e)
+				break
+			if qimg.isNull(): #no more images in stream
+				break
+			self.images.append(self.convertQImageToMat(qimg)) #appends copy
+
+		print "received %d images"%(len(self.images))
+		self.in_memory.unlock()
 
 
-_memory.lock();
+	def sendImages(self, images):
+		buf = QtCore.QBuffer()
+		buf.open(QtCore.QBuffer.WriteOnly)
+		datastream = QtCore.QDataStream(buf)
+		for img in images:
+			datastream << img
+
+		self.updateCommStatus(buf.size())
+		print "image width: %d"%(images[0].width())
+		
+		while True:
+			if self.out_memory.attach():
+				print("out memory attached")
+				break
+
+		self.out_memory.lock()
+		try:
+			self.out_memory.data()[:] = buf.data().data()
+		except Exception as e:
+			print(e)
+
+		self.out_memory.unlock()
+
+
+	def getImages(self):
+		return self.images
+
+	def convertMatToQImage(self, image):
+		return QtGui.QImage(image.data,image.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+
+	def convertQImageToMat(self, incomingImage):
+		'''  Converts a QImage into an numpy mat format  '''
+
+		incomingImage = incomingImage.convertToFormat(QtGui.QImage.Format_RGB888)
+
+		width = incomingImage.width()
+		height = incomingImage.height()
+
+		ptr = incomingImage.bits()
+		ptr.setsize(incomingImage.byteCount())
+		arr = np.array(ptr).reshape(height, width, 3)  #  Copies the data
+		return arr
+
+	def updateCommStatus(self, this_status):
+		buf = QtCore.QBuffer()
+		buf.open(QtCore.QBuffer.ReadWrite)
+
+		readStatus = self.readCommStatus()
+		status = QtCore.QPoint(readStatus.x(), this_status)
+
+		datastream = QtCore.QDataStream(buf)
+		datastream << status
+
+		self.comm_memory.lock()
+		self.comm_memory.data()[:] = buf.data().data()
+		self.comm_memory.unlock()
+
+	def readCommStatus(self):
+		status = QtCore.QPoint()
+		buf = QtCore.QBuffer()
+		datastream = QtCore.QDataStream(buf)
+
+		self.comm_memory.lock()
+		buf.setData(self.comm_memory.constData())
+		buf.open(QtCore.QBuffer.ReadOnly)
+		datastream >> status
+		self.comm_memory.unlock()
+
+		print status
+
+		return status
+
+	def __del__(self):
+		self.in_memory.detach()
+		self.out_memory.detach()
+		self.comm_memory.detach()
+		print "calling destructor"
+
+
 try:
-	qimg = QtGui.QImage()
-	buf = QtCore.QBuffer()
-	datastream = QtCore.QDataStream(buf)
-	buf.setData(_memory.constData())
-	buf.open(QtCore.QBuffer.ReadOnly)
-	datastream >> qimg
+	ipchandler = IPCHandler('process_example')
+	ipchandler.receiveImages()
+	images = ipchandler.getImages()
+	ipchandler.sendImages([ipchandler.convertMatToQImage(img) for img in images])
+	del ipchandler
 except Exception as e:
 	print(e)
 
-
-_memory.unlock();
-print("memory unlocked")
-
-try:
-	qimg.save("out.png")
-except Exception as e:
-	print(e)
-
-print("image saved")
-
-_memory.detach();
+print "finished"
